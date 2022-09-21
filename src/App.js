d754307f512dd5e69613a90c35ab1e6da4aabeae
itsoluwatobby <oluwasamuel050@gmail.com>
2022-09-21 09:47:11 +0100

    Components created and also routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import ForgotPassword from './pages/ForgotPassword';
+import Home from './pages/Home';
+import Missing from './pages/Missing';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <>
-      <h1 className='bg-orange-600'>I'm ready</h1>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/offers' element={<Offers />} />
+      <Route path='*' element={<Missing />} />
+    </Routes>
     </>
   );
 }
---
+++ src/pages/Missing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Missing = () => {
+  return (
+    <div>
+      Page not found
+    </div>
+  );
+}
+
+export default Missing;
